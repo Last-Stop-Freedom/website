cc9002c7fab3c85998b41c7b70f39e49ca9ec257
Alex Wilmer <djazium@gmail.com>
2020-10-11 22:42:59 -0400

    alt prop

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={Logo} />
+        <img src={Logo} alt="logo" />
         <p>
           LAST STOP: FREEDOM
         </p>
