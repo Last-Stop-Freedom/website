08c844511bdf1388956c8caf3a0965fe06bed8d2
Alex Wilmer <djazium@gmail.com>
2020-10-12 21:16:49 -0400

    meeting info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import Logo from './logo.svg'
 
+let link = 'https://us02web.zoom.us/j/87994984765'
+let linkProps = { target: "_blank", rel: "noopener noreferrer" }
+
 function App() {
   return (
     <div className="App">
@@ -18,8 +21,14 @@ function App() {
           }}
         />
         <em>There is a solution.</em>
-        <br />
-        <br />
+        <div className="info">
+          <p>Last Stop: Freedom is a group of alcoholics who meet once a
+        week to discuss a program of recovery as it pertains to the book Alcoholics Anonymous.</p>
+          <p>The meeting is currently being held every <b>Sunday @ 7:30pm EST</b>, online using Zoom.</p>
+
+          <p><a href={link} {...linkProps}>Click here to join the meeting<  /a> or enter the Meeting ID below into Zoom.</p>
+            <p>Zoom Meeting ID: 87994984765</p>
+        </div>
       </header>
     </div>
   );
